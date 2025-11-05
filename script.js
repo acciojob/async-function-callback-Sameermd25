a27b257072b63bba9ead335a697d95a63d5cf57c
Sameermd25 <149755097+Sameermd25@users.noreply.github.com>
2025-11-05 18:46:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 const url = "https://jsonplaceholder.typicode.com/posts/1";
 
+const btn=document.getElementById("btn")
+const outputDiv = document.getElementById("output");
 //your JS code here. If required.
+async function getData(callback){
+	try{
+		const data=await fetch(url);
+		const jsonData=await data.json();
+		callback(jsonData)
+	}catch(err){
+		console.log("error",err)
+	}
+}
+function callbackFunction(jsonData) {
+	outputDiv.innerHTML=`<h3>${jsonData.title}</h3>
+        <p>${jsonData.body}</p>`
+}
+
+btn.addEventListener("click",()=>{
+	getData(callbackFunction)
+})
